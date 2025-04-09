6984418b9507733246b8b4dfc7439e05185d3f86
ChannanagoudaM <channanagoudagouda51@gmail.com>
2025-04-09 06:09:54 -0700

    created online bus ticket booking repo

---
+++ BusInventoryService/src/main/java/com/example/BusInventoryService/BusInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusInventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ BusInventoryService/src/test/java/com/example/BusInventoryService/BusInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusInventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
